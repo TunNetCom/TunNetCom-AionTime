3a14c1dba3695067b66406c8dc93c50c8e663f3d
marwen saad <marwen.saad@outlook.com>
2024-05-05 18:54:32 +0100

    Feature AB12 : setup styleCop rules

--- src/TunNetCom.AionTime.AzureDevopsService/TunNetCom.AionTime.AzureDevopsService.API/Clients/AzureDevOpsClient.WorkItem.cs
+++ src/TunNetCom.AionTime.AzureDevopsService/TunNetCom.AionTime.AzureDevopsService.API/Clients/AzureDevOpsClient.WorkItem.cs
@@ -4,9 +4,9 @@ public partial class AzureDevOpsClient
 {
     public async Task<OneOf<WiqlResponses?, WiqlBadRequest?>> GetWiqlResponses(WiqlRequest wiqlRequest)
     {
-        _logger.LogInformation("Get Work Item for {Organisation}", wiqlRequest.Organization);
+        logger.LogInformation("Get Work Item for {Organisation}", wiqlRequest.Organization);
 
-        using HttpResponseMessage response = await _httpClient.PostAsJsonAsync(
+        using HttpResponseMessage response = await httpClient.PostAsJsonAsync(
         @$"/{wiqlRequest.Organization}/{wiqlRequest.Project}/{wiqlRequest.Team}/_apis/wit/wiql?api-version={wiqlRequest.ApiVersion}&top=10",
         wiqlRequest);
 
