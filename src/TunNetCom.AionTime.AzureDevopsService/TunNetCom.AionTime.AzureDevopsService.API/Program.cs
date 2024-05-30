72b24fe009f2a720efc127e49beea6979c5dec13
marwen saad <marwen.saad@outlook.com>
2024-05-30 22:14:28 +0100

    Feature AB#13 AB#33 : Add jaeger , OTL, promothese

--- src/TunNetCom.AionTime.AzureDevopsService/TunNetCom.AionTime.AzureDevopsService.API/Program.cs
+++ src/TunNetCom.AionTime.AzureDevopsService/TunNetCom.AionTime.AzureDevopsService.API/Program.cs
@@ -1,7 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Serilog.Events;
+using Serilog.Sinks.Grafana.Loki;
 using System.Runtime.CompilerServices;
 using TunNetCom.AionTime.AzureDevopsService.API.Clients.Settings;
 using TunNetCom.AionTime.AzureDevopsService.API.Data;
@@ -15,6 +21,41 @@ Log.Logger = new LoggerConfiguration()
 try
 {
     WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+    _ = builder.Logging
+    .AddOpenTelemetry(options =>
+    {
+        options.IncludeFormattedMessage = true;
+        options.IncludeScopes = true;
+
+        ResourceBuilder resBuilder = ResourceBuilder.CreateDefault();
+        _ = resBuilder.AddService(typeof(Program).Assembly.GetName().Name ?? "AzureDevopsService");
+        _ = options.SetResourceBuilder(resBuilder);
+
+        _ = options.AddOtlpExporter();
+    });
+    _ = builder.Services.Configure<AspNetCoreTraceInstrumentationOptions>(options =>
+    {
+        // Filter out instrumentation of the Prometheus scraping endpoint.
+        options.Filter = ctx => ctx.Request.Path != "/metrics";
+    });
+
+    _ = builder.Services.AddOpenTelemetry()
+        .ConfigureResource(b =>
+        {
+            _ = b.AddService(typeof(Program).Assembly.GetName().Name ?? "AzureDevopsService");
+        })
+        .WithTracing(b => b
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddEntityFrameworkCoreInstrumentation()
+            .AddSource(typeof(Program).Assembly.GetName().Name ?? "AzureDevopsService")
+            .AddOtlpExporter())
+        .WithMetrics(b => b
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddRuntimeInstrumentation()
+            .AddProcessInstrumentation()
+            .AddPrometheusExporter());
 
     Log.Information("Starting web host");
     _ = builder.Services.AddEndpointsApiExplorer();
@@ -37,6 +78,7 @@ try
         _ = app.UseSwaggerUI();
     }
 
+    _ = app.MapPrometheusScrapingEndpoint();
     app.AddTestEndpoints();
 
     _ = app.UseExceptionHandler();
--- src/TunNetCom.AionTime.TimeLogService/TunNetCom.AionTime.TimeLogService.Budget.API/Program.cs
+++ src/TunNetCom.AionTime.TimeLogService/TunNetCom.AionTime.TimeLogService.Budget.API/Program.cs
@@ -1,3 +1,8 @@
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Serilog.Events;
 using TunNetCom.AionTime.TimeLogService.API.Middelware;
 using TunNetCom.AionTime.TimeLogService.Infrastructure.AionTimeContext;
@@ -11,6 +16,42 @@ Log.Logger = new LoggerConfiguration()
 try
 {
     WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+    _ = builder.Logging
+    .AddOpenTelemetry(options =>
+    {
+        options.IncludeFormattedMessage = true;
+        options.IncludeScopes = true;
+
+        ResourceBuilder resBuilder = ResourceBuilder.CreateDefault();
+        _ = resBuilder.AddService(typeof(Program).Assembly.GetName().Name ?? "TimeLogService");
+        _ = options.SetResourceBuilder(resBuilder);
+
+        _ = options.AddOtlpExporter();
+    });
+    _ = builder.Services.Configure<AspNetCoreTraceInstrumentationOptions>(options =>
+    {
+        // Filter out instrumentation of the Prometheus scraping endpoint.
+        options.Filter = ctx => ctx.Request.Path != "/metrics";
+    });
+
+    _ = builder.Services.AddOpenTelemetry()
+        .ConfigureResource(b =>
+        {
+            _ = b.AddService(typeof(Program).Assembly.GetName().Name ?? "TimeLogService");
+        })
+        .WithTracing(b => b
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddEntityFrameworkCoreInstrumentation()
+            .AddSource(typeof(Program).Assembly.GetName().Name ?? "TimeLogService")
+            .AddOtlpExporter())
+        .WithMetrics(b => b
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddRuntimeInstrumentation()
+            .AddProcessInstrumentation()
+            .AddPrometheusExporter());
+
     _ = builder.Services.AddHttpContextAccessor();
     _ = builder.Host.UseSerilog((ctx, cfg) => cfg.ReadFrom.Configuration(ctx.Configuration));
     _ = builder.Services.AddControllers();
@@ -35,6 +76,7 @@ try
         _ = app.UseSwaggerUI();
     }
 
+    _ = app.MapPrometheusScrapingEndpoint();
     _ = app.UseHttpsRedirection();
     _ = app.UseExceptionHandler();
     _ = app.UseAuthorization();
