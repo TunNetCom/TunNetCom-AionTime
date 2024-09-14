75c41aabe99c5c8a473f608fe29337f13db3f2cb
marwen saad <marwen.saad@outlook.com>
2024-09-14 20:22:32 +0100

    scoped namespace

--- src/AzureDevopsService/AzureDevopsService.Application/AzureDevopsExternalResourceService/OrganizationProjectService/ProjectService.cs
+++ src/AzureDevopsService/AzureDevopsService.Application/AzureDevopsExternalResourceService/OrganizationProjectService/ProjectService.cs
@@ -1,34 +1,33 @@
-﻿namespace AzureDevopsService.Application.AzureDevopsExternalResourceService.OrganizationProjectService
+﻿namespace AzureDevopsService.Application.AzureDevopsExternalResourceService.OrganizationProjectService;
+
+public class ProjectService(HttpClient httpClient, ILogger<ProjectService> logger) : IProjectService
 {
-    public class ProjectService(HttpClient httpClient, ILogger<ProjectService> logger) : IProjectService
+    private readonly HttpClient _httpClient = httpClient;
+    private readonly ILogger<ProjectService> _logger = logger;
+
+    public async Task<OneOf<AllProjectResponce?, CustomProblemDetailsResponce?>> AllProjectUnderOrganization(AllProjectUnderOrganizationRequest request)
     {
-        private readonly HttpClient _httpClient = httpClient;
-        private readonly ILogger<ProjectService> _logger = logger;
+        HttpClientHelper.SetAuthHeader(_httpClient, request.Path);
 
-        public async Task<OneOf<AllProjectResponce?, CustomProblemDetailsResponce?>> AllProjectUnderOrganization(AllProjectUnderOrganizationRequest request)
+        HttpResponseMessage projectsResult = await _httpClient.GetAsync("_apis/profile/profiles/me?api-version=7.0");
+
+        if (projectsResult.StatusCode == HttpStatusCode.OK)
         {
-            HttpClientHelper.SetAuthHeader(_httpClient, request.Path);
-
-            HttpResponseMessage projectsResult = await _httpClient.GetAsync("_apis/profile/profiles/me?api-version=7.0");
-
-            if (projectsResult.StatusCode == HttpStatusCode.OK)
-            {
-                AllProjectResponce? projects = await projectsResult.Content.ReadFromJsonAsync<AllProjectResponce>();
+            AllProjectResponce? projects = await projectsResult.Content.ReadFromJsonAsync<AllProjectResponce>();
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
-                projects.Path = request.Path;
+            projects.Path = request.Path;
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
-                projects.Email = request.Email;
-                return projects;
-            }
-
-            _logger.LogError(await projectsResult.Content.ReadAsStringAsync());
-            return new CustomProblemDetailsResponce()
-            {
-                Path = request.Path,
-                Email = request.Email,
-                Status = (int)projectsResult.StatusCode,
-                Detail = AzureResponseMessage.VerifyAzureDevOpsKeyOrOrgName,
-            };
+            projects.Email = request.Email;
+            return projects;
         }
+
+        _logger.LogError(await projectsResult.Content.ReadAsStringAsync());
+        return new CustomProblemDetailsResponce()
+        {
+            Path = request.Path,
+            Email = request.Email,
+            Status = (int)projectsResult.StatusCode,
+            Detail = AzureResponseMessage.VerifyAzureDevOpsKeyOrOrgName,
+        };
     }
 }
