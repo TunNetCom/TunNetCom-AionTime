ded21d7f7b7ed5698a4d397d3cbbf3af658f8864
msaad <msaad@filiassur.com>
2024-09-20 11:37:56 +0100

    Modify Database

--- src/TimeLogService/TimeLogService.DataBase/dbo/Tables/Organization.sql
+++ src/TimeLogService/TimeLogService.DataBase/dbo/Tables/Organization.sql
@@ -1,17 +1,11 @@
 ﻿CREATE TABLE [dbo].[Organization]
 (
 	[Id] INT  IDENTITY (1, 1) PRIMARY KEY NOT NULL,
-    [UserId] NVARCHAR(100) not null,
 	[Name] Varchar(255) not null, 
     [AccountId] NVARCHAR(100) NOT NULL unique, 
     [AccountUri] VARCHAR(200) NOT NULL,
-    [IsAionTimeApproved] BIT NOT NULL 
-
-	CONSTRAINT FKUserOrganization 
-        FOREIGN KEY (UserId) 
-        REFERENCES [dbo].[User] (UserId) 
-        ON DELETE CASCADE
-        ON UPDATE CASCADE, 
+    [IsAionTimeApproved] BIT NOT NULL,
+    [CreationDate] datetime not null
 )
 
 GO
