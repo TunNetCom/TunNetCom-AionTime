79fb99f3ab5bb656e9314f30670090a379d9c9da
Nieze Ben Mansour <nieze.benmansour@outlook.fr>
2024-12-06 14:28:29 +0100

    fix startup issues

--- src/TimeLogIdentityService/IdentityService.Application/ApplicationServiceRegistration.cs
+++ src/TimeLogIdentityService/IdentityService.Application/ApplicationServiceRegistration.cs
@@ -28,7 +28,6 @@ public static class ApplicationServiceRegistration
                 });
 
                 cfg.UseNewtonsoftJsonSerializer();
-                cfg.ConfigureEndpoints(context);
 
                 cfg.ReceiveEndpoint("ProfileUserResponce", e =>
                 {
